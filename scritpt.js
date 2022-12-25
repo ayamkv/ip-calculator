f7ce69604056eee20aad2b029926ee4f9015fb88
ayamkv <65036385+ayamkv@users.noreply.github.com>
2022-12-25 07:21:47 +0000

    fun, add backspace to prev inputs

--- scritpt.js
+++ scritpt.js
@@ -1,11 +1,14 @@
-const inputs = document.querySelectorAll("input[type=number]");
-
-inputs.forEach((input) => {
-  input.min = 1;
+document.querySelectorAll("input[type=number]").forEach((input) => {
+  input.min = 0;
   input.max = 100;
   input.setAttribute("onkeyup", "enforceMinMax(this)");
   input.placeholder = "Isi dengan nomor 1 - 100";
 });
+
+document.querySelectorAll(".inputs").forEach((div) => {
+  div.setAttribute("tabindex", "0");
+});
+
 
 function enforceMinMax(inputElement) {
   const value = inputElement.value;
@@ -56,22 +59,76 @@ function calculateResult() {
 
   // Add the "animated" class to the result element
   document.getElementById("result").classList.add("animated");
+  document.getElementById("raw-c").classList.add("result");
   
   // Wait for 1 second before removing the "animated" class
   setTimeout(function () {
     document.getElementById("result").classList.remove("animated");
+    document.getElementById("raw-c").classList.remove("result");
   }, 1000);
 }
-function handleKeydown(event) {
+
+let backspaceCounter = 0;
+
+function handleKeydown(event, button) {
+  const inputs2 = document.querySelectorAll(".inputs input");
+  const inputsArray = Array.from(inputs2);
+
+
+  if (event.key === "Backspace" && event.target.value === "") {
+    // Get the index of the current input
+    const index = inputsArray.indexOf(event.target);
+   // Increment the counter
+   backspaceCounter++;
+
+   // If the counter is less than 2
+   if (backspaceCounter < 1) {
+     // Cancel the default behavior of the backspace key
+     event.preventDefault();
+   }
+   // If the counter is 2 or more
+   else {
+     // Reset the counter
+     backspaceCounter = 0;
+
+     // Get the index of the current input
+     const index = inputsArray.indexOf(event.target);
+
+     // If the current input is not the first input
+     if (index > 0) {
+       // Move focus to the previous input
+       inputsArray[index - 1].focus();
+     }
+   }
+ }
   // If the enter key was pressed
-  if (event.key === "Enter") {
-    // Trigger the calculate button
-    document.getElementById("calculate").click();
+  else if (event.key === "Enter") {
+    // Get the index of the current input
+    const index = inputsArray.indexOf(event.target);
+
+    // If the current input is not the last input
+    if (index < inputsArray.length - 1) {
+      // Move focus to the next input
+      inputsArray[index + 1].focus();
+    }
+    // If the current input is the last input
+    else {
+
+      // Trigger the calculate button
+      button.click();
+
+
+    }
   }
 }
 
 // Add an event listener to the document to listen for keydown events
-document.addEventListener("keydown", handleKeydown);
+// Get the calculate button element
+const button = document.getElementById("calculate");
+
+// Add an event listener to the document to listen for keydown events
+document.addEventListener("keydown", (event) => handleKeydown(event, button));
+
 
 // Add an event listener to the calculate button
 document.getElementById("calculate").addEventListener("click", calculateResult);
